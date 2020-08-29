0470d96c5691184a05107a4b8feb57b2e6702996
小H <290911574@qq.com>
2020-08-29 17:38:29 +0800

    修改测试报告的名称为固定值

--- run_suite.py
+++ run_suite.py
@@ -14,7 +14,8 @@ suite.addTest(unittest.makeSuite(trust))
 suite.addTest(unittest.makeSuite(tender))
 suite.addTest(unittest.makeSuite(test_tender_process))
 
-report_file = app.BASE_DIR + "/report/report{}.html".format(time.strftime("%Y%m%d-%H%M%S"))
+#report_file = app.BASE_DIR + "/report/report{}.html".format(time.strftime("%Y%m%d-%H%M%S"))
+report_file = app.BASE_DIR + "/report/report.html"
 with open(report_file,'wb') as f:
     runner = HTMLTestRunner(f,title="P2P金融项目接口测试报告",description="test")
     runner.run(suite)
